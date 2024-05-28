5e59c37eae3159463dbcec63983a18e9bb663316
Gustavio <gust3371@gmail.com>
2024-05-28 17:25:49 +0200

    Attempt new login handling

--- src/login.php
+++ src/login.php
@@ -1,10 +1,51 @@
 <?php
 session_start();
 
-function validate_password($password, $hash) {
-    // Check if the provided password matches the hashed password
-    echo 'Hash: ' . md5($password);
-    return md5($password) === $hash;
+function validate_apr1_md5($password, $hash) {
+    $salt = substr($hash, 6, 8);  // Extract the salt from the hash
+    return $hash === apr1_md5($password, $salt);
+}
+
+function apr1_md5($password, $salt) {
+    $len = strlen($password);
+    $text = $password . '$apr1$' . $salt;
+    $bin = pack("H32", md5($password . $salt . $password));
+
+    for ($i = $len; $i > 0; $i -= 16) {
+        $text .= substr($bin, 0, min(16, $i));
+    }
+
+    for ($i = $len; $i > 0; $i >>= 1) {
+        $text .= ($i & 1) ? chr(0) : $password[0];
+    }
+
+    $bin = pack("H32", md5($text));
+
+    for ($i = 0; $i < 1000; $i++) {
+        $new = ($i & 1) ? $password : $bin;
+        if ($i % 3) {
+            $new .= $salt;
+        }
+        if ($i % 7) {
+            $new .= $password;
+        }
+        $new .= ($i & 1) ? $bin : $password;
+        $bin = pack("H32", md5($new));
+    }
+
+    $tmp = '';
+    for ($i = 0; $i < 5; $i++) {
+        $k = ($i + 6) % 8;
+        $j = ($i + 12) % 8;
+        $tmp = $bin[$k] . $bin[$j] . $tmp;
+    }
+    $tmp = chr(0) . chr(0) . $bin[11] . $tmp;
+
+    $result = '$apr1$' . $salt . '$';
+    $tmp = base64_encode($tmp);
+    $tmp = str_replace(['+', '/', '='], ['.', '', ''], $tmp);
+
+    return $result . substr($tmp, 0, 22);
 }
 
 function get_htpasswd_credentials($file_path) {
@@ -22,22 +63,21 @@ function get_htpasswd_credentials($file_path) {
 }
 
 // Path to the .htpasswd file
-$htpasswd_file = '/etc/apache2/.temppasswd';
+$htpasswd_file = '/etc/apache2/.htpasswd';
 
 $password = $_POST['password'];
 $credentials = get_htpasswd_credentials($htpasswd_file);
 
 $user_password_hash = isset($credentials['user']) ? $credentials['user'] : null;
 $admin_password_hash = isset($credentials['admin']) ? $credentials['admin'] : null;
-echo 'User hash: ' . $user_password_hash . ' Admin hash: ' . $admin_password_hash;
 
-if ($user_password_hash && validate_password($password, $user_password_hash)) {
+if ($user_password_hash && validate_apr1_md5($password, $user_password_hash)) {
     $_SESSION['role'] = 'user';
-    header('Location: /main/index.html');
+    header('Location: /User/index.html');
     exit;
-} elseif ($admin_password_hash && validate_password($password, $admin_password_hash)) {
+} elseif ($admin_password_hash && validate_apr1_md5($password, $admin_password_hash)) {
     $_SESSION['role'] = 'admin';
-    header('Location: /main/index.html');
+    header('Location: /Admin/index.html');
     exit;
 } else {
     echo "Invalid password.";
