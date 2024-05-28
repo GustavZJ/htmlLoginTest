24b18aa6ce657b72d05a4987e6c92a01ae9be523
Gustavio <gust3371@gmail.com>
2024-05-28 17:09:31 +0200

    Add update and install script

---
+++ install.sh
@@ -0,0 +1,3 @@
+cp logintest.conf /etc/apache2/sites-available/logintest.conf
+
+sudo a2ensite logintest.conf
---
+++ update.sh
@@ -0,0 +1,2 @@
+git pull https://github.com/GustavZJ/htmllogintest.git
+systemctl restart apache
