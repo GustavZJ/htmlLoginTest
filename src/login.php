53a867e4c696c15dd2a7108990b014105ac53bb5
Gustavio <gust3371@gmail.com>
2024-05-28 17:52:20 +0200

    Debug

--- src/login.php
+++ src/login.php
@@ -29,6 +29,10 @@ function verify_apr1_md5_password($password, $hashed_password) {
     $passParts = explode('$', $hashed_password);
     $salt = $passParts[2];
     $hashed = crypt_apr1_md5($password, $salt);
+    echo 'Password: '.$password.'<br>';
+    echo 'Salt: '.$salt.'<br>';
+    echo 'Hash: '.$hashed.'<br>';
+    echo 'Stored hash: '.$hashed_password.'<br>';
     return $hashed === $hashed_password;
 }
 
